83e30d6eb026df43bbe65c71f0a5905350ec1122
Kazuya Saso <kazsan0215@gmail.com>
2025-05-19 00:05:50 +0900

    create api

--- services/dex/cmd/main.go
+++ services/dex/cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"github.com/rendaman0215/poke_api/services/dex/internal/handler"
 	"github.com/rendaman0215/poke_api/services/dex/internal/logger"
 )
 
 func main() {
+	// Initialize the logger
 	logger.InitLogger()
+
+	// Start the server
+	handler.Handler()
 }
--- services/dex/internal/handler/handler.go
+++ services/dex/internal/handler/handler.go
@@ -1,14 +1,14 @@
 package handler
 
-package main
-
 import (
 	"context"
+	"errors"
 	"fmt"
-	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"connectrpc.com/connect"
 	"golang.org/x/net/http2"
@@ -16,30 +16,54 @@ import (
 
 	dexv1 "github.com/rendaman0215/poke_api/schema/gen/go/dex/v1"
 	"github.com/rendaman0215/poke_api/schema/gen/go/dex/v1/dexv1connect"
+	service "github.com/rendaman0215/poke_api/services/dex/internal/service"
 )
-
-const port = ":8080"
 
 type dexService struct{}
 
 func (w *dexService) GetImage(ctx context.Context, req *connect.Request[dexv1.GetImageRequest]) (*connect.Response[dexv1.GetImageResponse], error) {
-	log.Println("Request headers: ", req.Header())
-	res := connect.NewResponse(&dexv1.GetImageResponse{
-		Url: "https://zukan.pokemon.co.jp/zukan-api/up/images/index/7b705082db2e24dd4ba25166dac84e0a.png",
-	})
+	slog.Info("GetImage", "req", req.Msg)
+	res, err := service.GetImage(req)
+	if err != nil {
+		slog.Error("GetImage", "err", err)
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
 	res.Header().Set("Weather-Version", "v1")
 	return res, nil
 }
 
-func serviceStart() {
+func Handler() {
+	port := ":8080"
+
 	ds := &dexService{}
 	mux := http.NewServeMux()
 	path, handler := dexv1connect.NewDexImageServiceHandler(ds)
 	mux.Handle(path, handler)
+	server := &http.Server{
+		Addr:    fmt.Sprintf("localhost%s", port),
+		Handler: h2c.NewHandler(mux, &http2.Server{}),
+	}
+	idleConnsClosed := make(chan struct{})
 
-	logger.Info("Starting server", "port", port)
-	http.ListenAndServe(
-		fmt.Sprintf("localhost%s", port),
-		h2c.NewHandler(mux, &http2.Server{}),
-	)
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM) // SIGINT, SIGTERM を検知する
+		<-c
+
+		slog.Info("Server is shutting down...")
+		if err := server.Shutdown(context.Background()); err != nil {
+			slog.Error("Server Shutdown", "err", err)
+			close(idleConnsClosed)
+			return
+		}
+		slog.Info("Server is shut down")
+		close(idleConnsClosed)
+	}()
+
+	slog.Info("Starting server", "port", port)
+	if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		slog.Error("HTTP server ListenAndServe", "err", err)
+	}
+
+	<-idleConnsClosed // Shutdown処理が完了するまで待機する
 }
